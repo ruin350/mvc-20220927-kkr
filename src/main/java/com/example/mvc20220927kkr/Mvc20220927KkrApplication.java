49c98b10027336d7eab94332ff5eef2f037b3f52
ruin350 <kkr583@naver.com>
2022-09-28 21:00:10 +0900

    day1

---
+++ src/main/java/com/example/mvc20220927kkr/Mvc20220927KkrApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvc20220927kkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc20220927KkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc20220927KkrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mvc20220927kkr/Mvc20220927KkrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvc20220927kkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc20220927KkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
